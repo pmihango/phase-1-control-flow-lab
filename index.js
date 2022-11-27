f91d116910390c1259f5f8b0c77282df4e4dcff3
pmihango <pmihango@gmail.com>
2022-11-27 13:52:49 +0300

    flow control

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
-
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(ride){
+  if(ride <= 400) {
+      return 'This one is on me!'}
+    else  if (ride >= 2500) {
+      return 'No can do.' }
+      else {
+          return 'I will gladly take your thirty bucks.'}
+  }
+  function ternaryCheckCity(city){
+    return city === 'NYC'?"Ok, sounds good." :"No go."
+    }
+  const tip = 400;
+  function switchOnCharmFromTip(tip){
+    switch(tip) {
+      case 'generous':
+        return "Thank you so much."
+      case 'not as generous':
+        return "Thank you."
+      default:
+        return "Bye."
+    }
+  }
